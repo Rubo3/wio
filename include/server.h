97e22bf27b36ffb1c37feb86fbfca6ca6a1de2f7
Drew DeVault <sir@cmpwn.com>
2019-01-09 22:51:48 -0500

    Handle new outputs

--- include/server.h
+++ include/server.h
@@ -3,12 +3,25 @@
 #include <wayland-server.h>
 #include <wlr/backend.h>
 #include <wlr/render/wlr_renderer.h>
+#include <wlr/types/wlr_output.h>
 
 struct wio_server {
 	struct wl_display *wl_display;
 
 	struct wlr_backend *backend;
 	struct wlr_renderer *renderer;
+
+	struct wl_list outputs;
+
+	struct wl_listener new_output;
+};
+
+struct wio_output {
+	struct wl_list link;
+	struct wio_server *server;
+	struct wlr_output *wlr_output;
+
+	struct wl_listener frame;
 };
 
 #endif
